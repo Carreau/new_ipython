98aaa0859c4f044257cf281fe1dd45c2d2dfb3a1
M Bussonnier <bussonniermatthias@gmail.com>
2025-12-09 22:10:38 +0100

    ,

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  base: '/new_ipython'
-  output: 'static',
+  base: "/new_ipython",
+  output: "static",
 });
